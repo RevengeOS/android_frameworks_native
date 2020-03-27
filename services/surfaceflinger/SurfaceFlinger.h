e9bd49dbca51d7cad9f2c86252225e79810ecafd
Lucchetto <lucchetto.tie@live.com>
2020-03-27 15:28:38 +0100

    Revert "Revert "Fix drag and drop (2/3)""
    This reverts commit 5845eafaad804ffe8088cd34cdb52f6f9f9282de.
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- services/surfaceflinger/SurfaceFlinger.h
+++ services/surfaceflinger/SurfaceFlinger.h
@@ -556,6 +556,7 @@ private:
     void updateInputFlinger();
     void updateInputWindowInfo();
     void commitInputWindowCommands() REQUIRES(mStateLock);
+    void executeInputWindowCommands();
     void setInputWindowsFinished();
     void updateCursorAsync();
 
